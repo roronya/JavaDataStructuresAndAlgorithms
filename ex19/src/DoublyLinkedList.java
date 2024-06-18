8226122cc3d70705c0c786ae44d8ba1edd278d5c
roronya <roronya628@gmail.com>
2024-06-18 20:02:40 +0900

    add ex19

---
+++ ex19/src/DoublyLinkedList.java
@@ -0,0 +1,62 @@
+public class DoublyLinkedList {
+    Node head;
+    Node tail;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    DoublyLinkedList(int value) {
+        var node = new Node(value);
+        head = node;
+        tail = node;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+}
+
+
---
+++ ex19/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+
+        myDLL.printAll();
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 7
+            Tail: 7
+            Length: 1
+
+            Linked List:
+            7
+
+        */
+
+    }
+
+}
