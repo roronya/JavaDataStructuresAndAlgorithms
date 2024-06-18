2c2fd3ecd29d9d6a014fa579f5a993e18acc5205
roronya <roronya628@gmail.com>
2024-06-18 20:31:23 +0900

    add ex24

---
+++ ex24/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+
+        System.out.println(myDLL.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+    }
+
+}
