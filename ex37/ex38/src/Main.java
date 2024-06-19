7b1d55b9a4d422c38b7a9756aee05d36eb34f01b
roronya <roronya628@gmail.com>
2024-06-19 10:46:12 +0900

    add ex38

---
+++ ex37/ex38/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack myStack = new Stack();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStack.printStack();
+
+        /*
+            EXPECTED OUTPUT:
+			3
+			2
+			1
+
+        */
+
+    }
+
+}
