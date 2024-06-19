d12f10e3b216c15637a5188b74619bab253fdf92
roronya <roronya628@gmail.com>
2024-06-19 10:23:07 +0900

    add ex35

---
+++ ex35/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(4);
+
+        myQueue.printAll();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            First: 4
+            Last: 4
+            Length: 1
+
+            Queue:
+            4
+
+        */
+
+    }
+
+}
