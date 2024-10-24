f0a5f4a4b6a4cebec0456dd70d2d33c631de6805
roronya <roronya628@gmail.com>
2024-10-24 19:58:37 +0900

    add ex69

---
+++ ex69_heap_minheap_insert/src/MinHeap.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinHeap {
+    private List<Integer> heap;
+
+    public MinHeap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    // WRITE THE INSERT METHOD HERE //
+    public void insert(int value) {
+        heap.add(value);
+        var currentIndex = heap.size() - 1;
+        while(true) {
+            var parentIndex = parent(currentIndex);
+            if (heap.get(currentIndex) >= heap.get(parentIndex)) {
+                break;
+            }
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+        }
+    }
+
+}
