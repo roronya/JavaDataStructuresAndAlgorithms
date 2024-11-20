fb509c55bb90771ad1366602d8904d3b321c3e37
roronya <roronya628@gmail.com>
2024-11-21 00:57:52 +0900

    add ex96

---
+++ ex96_quick_sort/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+
+public class Main {
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for (int i = pivotIndex + 1; i <= endIndex; i++) {
+            if (array[i] < array[pivotIndex]) {
+                swapIndex++;
+                swap(array, swapIndex, i);
+            }
+        }
+        swap(array, pivotIndex, swapIndex);
+
+        return swapIndex;
+    }
+
+
+    // WRITE QUICKSORT HELPER METHOD HERE //
+    public static void quickSortHelper(int[] a, int left, int right) {
+        // System.out.println(left + ":" + right);
+        // System.out.println(Arrays.toString(a));
+        if (left > right) {
+            return;
+        }
+        int mid = pivot(a, left, right);
+        quickSortHelper(a, left, mid - 1);
+        quickSortHelper(a, mid + 1, right);
+    }
+
+
+    public static void quickSort(int[] array) {
+        quickSortHelper(array, 0, array.length-1);
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4,6,1,7,3,2,5};
+
+        quickSort(myArray);
+
+        System.out.println( Arrays.toString( myArray ) );
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6, 7]
+
+         */
+
+    }
+
+}
